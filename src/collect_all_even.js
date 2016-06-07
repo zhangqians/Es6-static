f11523ff3d0767f1639c0863b24ac786759ef949
zhangqian <2545026281@qq.com>
2016-06-07 14:24:27 +0800

    first commit

---
+++ src/collect_all_even.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function collect_all_even(collection) {
+  var arr=new Array();
+  for(var i=0;i<collection.length;i++)
+  {
+	  if(collection[i]%2==0)
+	  arr.push(collection[i]);
+	  }
+      return arr;
+}
+
+module.exports = collect_all_even;
